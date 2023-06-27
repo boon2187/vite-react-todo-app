6f27d7fc1a7d9e2ce58825a91a05cf8b4a76248c
boon2187 <fumiya.mai.ito@gmail.com>
2023-06-28 07:55:01 +0900

    ログインを非同期処理に

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,9 +2,9 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
 import { Button } from "@chakra-ui/react";
 export const SignIn = () => {
-  function signInWithGoogle() {
+  async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider);
+    await signInWithPopup(auth, provider);
     // const currentUser = auth.currentUser?.displayName;
     // console.log(currentUser);
   }
