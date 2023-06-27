e4a944e0f16d3e5559e1a3cc91954836b7868730
boon2187 <fumiya.mai.ito@gmail.com>
2023-06-27 20:20:29 +0900

    Googleアカウントでログインするコンポーネントを作成

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,16 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase";
+import { Button } from "@chakra-ui/react";
+export const SignIn = () => {
+  function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+    const currentUser = auth.currentUser?.displayName;
+    console.log(currentUser);
+  }
+  return (
+    <div>
+      <Button onClick={signInWithGoogle}>Googleのアカウントでログイン</Button>
+    </div>
+  );
+};
