e474a5952935b4064640b0fc994f15e2fd31044e
boon2187 <fumiya.mai.ito@gmail.com>
2023-06-28 20:37:51 +0900

    SignOutコンポーネントを新規に作成

---
+++ src/components/SignOut.tsx
@@ -0,0 +1,14 @@
+import { auth } from "../firebase.ts";
+import { Button } from "@chakra-ui/react";
+
+function SignOut() {
+  console.log(auth.currentUser?.displayName);
+  return (
+    <div className="header">
+      <Button>サインアウト</Button>
+      <h3>{auth.currentUser?.displayName}</h3>
+    </div>
+  );
+}
+
+export default SignOut;
