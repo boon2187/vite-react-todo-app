efca4efcbb65248fddda8e4e1358e6d424b7b534
boon2187 <fumiya.mai.ito@gmail.com>
2023-06-28 20:13:54 +0900

    サインインしてなければサインインのボタンが出るようにToDoWrapperを改良。それにともない、App.tsxはもとに戻す

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { SignIn } from "./components/SignIn";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./firebase.ts";
+
 import { ToDoWrapper } from "./components/ToDoWrapper";
 
 function App() {
-  const [user] = useAuthState(auth);
-  return <>{user ? <ToDoWrapper /> : <SignIn />}</>;
+  return (
+    <>
+      <ToDoWrapper />
+    </>
+  );
 }
 
 export default App;
