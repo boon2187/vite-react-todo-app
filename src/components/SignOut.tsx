0a443e07f81e4e367d1450cb360df4fb0ef006af
boon2187 <fumiya.mai.ito@gmail.com>
2023-07-04 07:22:33 +0900

    Flexコンポーネントに置き換え

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -1,12 +1,19 @@
 import { auth } from "../firebase.ts";
-import { Button } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
 
 export const SignOut = () => {
   console.log(auth.currentUser?.displayName);
   return (
-    <div className="header">
-      <Button onClick={() => auth.signOut()}>サインアウト</Button>
-      <h3>{auth.currentUser?.displayName}</h3>
-    </div>
+    <Flex
+      bg="#8758ff"
+      color="white"
+      flexDirection="row-reverse"
+      alignItems="center"
+    >
+      <Button onClick={() => auth.signOut()} ml="20px">
+        サインアウト
+      </Button>
+      <Text>{auth.currentUser?.displayName}</Text>
+    </Flex>
   );
 };
