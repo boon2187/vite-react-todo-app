62eb6eef308c0c052777f002e427274f839aceb7
boon2187 <fumiya.mai.ito@gmail.com>
2023-07-04 16:44:35 +0900

    見た目を整える

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -5,22 +5,30 @@ import { UnlockIcon } from "@chakra-ui/icons";
 export const SignOut = () => {
   return (
     <Flex
-      bg="#8758ff"
+      bg="#1a1a40"
       color="white"
       flexDirection="row-reverse"
       alignItems="center"
       h="50px"
-      gap={2}
+      gap={5}
+      mb="30px"
     >
       <IconButton
+        colorScheme="purple"
         aria-label="Sign Out"
         icon={<UnlockIcon />}
         onClick={() => auth.signOut()}
       >
         サインアウト
       </IconButton>
-      <Text>{auth.currentUser?.displayName}</Text>
-      <Avatar name="login user name" src={`${auth.currentUser?.photoURL}`} />
+      <Text fontSize="xl" fontWeight="bold">
+        {auth.currentUser?.displayName}
+      </Text>
+      <Avatar
+        size="sm"
+        name="login user name"
+        src={`${auth.currentUser?.photoURL}`}
+      />
     </Flex>
   );
 };
