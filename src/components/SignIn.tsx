eed9b5e16a25811dfe275735100ed8a53dc6dac8
boon2187 <fumiya.mai.ito@gmail.com>
2023-09-06 17:59:12 +0900

    いろいろいじったけどもとに戻したはず

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,5 +1,5 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "../firebase.ts";
 import { Button, Flex } from "@chakra-ui/react";
 import { ArrowRightIcon } from "@chakra-ui/icons";
 
